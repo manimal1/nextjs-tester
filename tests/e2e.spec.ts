7c515c1399dd0313bbd10ef4fdfd5658b44409a8
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-01 00:09:54 +0200

    feat: add playwright with initial tests

---
+++ tests/e2e.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test'
+
+test('should navigate to the profile page', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.click('text=Profile');
+  await expect(page).toHaveURL('http://localhost:3000/profile');
+  await expect(page.locator('h1')).toContainText('Access Denied');
+});
+
+test.describe('posts', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto('http://localhost:3000/posts');
+  });
+  
+  test('posts page should load', async ({ page }) => {
+    await expect(page.locator('h1')).toContainText('Posts');
+  });
+
+  test('should navigate to the first post', async ({ page }) => {
+    await page.getByAltText('post-link').first().click();
+    await page.waitForURL('**/posts/*');
+
+    expect(page.url()).toContain('/posts/');
+  });
+});
