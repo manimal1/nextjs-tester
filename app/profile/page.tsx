c778e7e5e244ea8c61ecae778c6399d2471951f0
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-09-30 21:33:24 +0200

    feat: update profile page with new post form and protected route

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,14 +1,17 @@
-// import { ProtectedRoute } from '@/app/components';
-// import { CreatePostForm } from './components/CreatPostForm';
+import { ProtectedRoute } from '@/app/components';
+import { Container } from '@/ui';
+import { CreatePostForm } from './components/CreatPostForm';
 import { ProfileHeader } from './components/ProfileHeader';
 
 export default async function ProfileRoute() {
   return (
-    <>
+    <ProtectedRoute>
       <ProfileHeader />
       <div className="border border-black rounded divide-y-2 my-6" />
-      <h2 className="title-lg my-8">Create a new post</h2>
-      {/* <CreatePostForm /> */}
-    </>
+      <Container className="bg-base-300 rounded-lg">
+        <h2 className="title-lg mb-8">Create a new post</h2>
+        <CreatePostForm />
+      </Container>
+    </ProtectedRoute>
   );
 }
