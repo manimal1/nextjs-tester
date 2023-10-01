c42ba25e3f783a9fa65ad48ec8a18ec099f0dcd2
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-01 19:37:53 +0200

    refactor: use post component in post page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,8 +1,7 @@
-import dayjs from 'dayjs';
-import Image from 'next/image';
 import { HiArrowLeft } from 'react-icons/hi2';
 import { getPostById, getUserById } from '@/queries';
-import { Container, Link } from '@/ui';
+import { Link } from '@/ui';
+import { Post as PostBase } from '../components/Post';
 
 export default async function Post({ params: { id } }: { params: { id: string } }) {
   const post = await getPostById(id);
@@ -11,11 +10,6 @@ export default async function Post({ params: { id } }: { params: { id: string } 
     return null;
   }
 
-  const user = await getUserById(post.authorId);
-
-  const authorName = post.author ? post.author.name : 'Unknown author';
-  const publicationDate = dayjs(post.createdAt).format('DD/MM/YYYY');
-
   return (
     <>
       <Link href="/posts" className="relative">
@@ -23,29 +17,9 @@ export default async function Post({ params: { id } }: { params: { id: string } 
           alt="post-link"
           className="text-secondary h-4 w-4 absolute top-1 left-0 right-auto"
         />
-        <p className="text-secondary ml-4">Back to posts</p>
+        <p className="text-secondary ml-4 mb-4">Back to posts</p>
       </Link>
-      <section className="bg-base-300 rounded-lg mt-4">
-        <Container>
-          <div className="flex flex-col space-y-2">
-            <h2 className="heading">{post?.title}</h2>
-            <div className="flex items-center space-x-4">
-              <Image
-                src={user?.image ?? ''}
-                className="rounded-full w-8 h-8"
-                width={32}
-                height={32}
-                alt="avatar"
-              />
-              <div className="flex flex-col">
-                <small className="text-secondary">By {authorName}</small>
-                <small>{`published: ${publicationDate}`}</small>
-              </div>
-            </div>
-          </div>
-          <p className="mt-12">{post.content}</p>
-        </Container>
-      </section>
+      <PostBase post={post} />
     </>
   );
 }
