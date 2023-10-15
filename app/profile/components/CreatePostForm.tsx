d227f6c8f71385ee7cafe2826aecdbcd08f77fa7
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-15 17:03:31 +0200

    fix: create post form file name typo

--- app/profile/components/CreatePostForm.tsx
+++ app/profile/components/CreatePostForm.tsx
@@ -1,17 +1,15 @@
-'use client';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useSession } from 'next-auth/react';
-import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
-import { createPostAction, getUserByEmailAction } from '@/app/actions';
-import { Input, TextArea } from '@/hook-form';
-import { Button } from '@/ui';
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { createPostAction } from "@/app/actions";
+import { Input, TextArea } from "@/hook-form";
+import { Button } from "@/ui";
 
 const schema = z.object({
-  title: z.string().refine((value) => !!value, { message: 'Title is required', path: ['title'] }),
-  content: z
-    .string()
-    .refine((value) => !!value, { message: 'Content is required', path: ['content'] }),
+  title: z.string().refine((value) => !!value, { message: "Title is required", path: ["title"] }),
+  content: z.string().refine((value) => !!value, { message: "Content is required", path: ["content"] }),
 });
 
 type FormSchema = z.infer<typeof schema>;
@@ -21,41 +19,40 @@ export function CreatePostForm() {
 
   const methods = useForm<FormSchema>({
     defaultValues: {
-      title: '',
-      content: '',
+      title: "",
+      content: "",
     },
     resolver: zodResolver(schema),
   });
 
   const {
     handleSubmit,
-    formState: { isSubmitting, isValidating },
+    formState: { isSubmitting, isValidating, isValid },
     reset,
   } = methods;
 
   const onSubmit: SubmitHandler<FormSchema> = async (data) => {
     try {
       schema.parse(data);
-      const user = await getUserByEmailAction('/profile', session?.user?.email);
 
-      if (!user) {
-        throw new Error('User not found');
+      if (!session?.user) {
+        throw new Error("User not found");
       }
 
       await createPostAction({
         title: data.title,
         content: data.content,
-        authorId: user?.id,
-        path: '/profile',
+        authorId: session.user.id,
+        path: "/profile",
       });
 
       reset();
     } catch (error) {
-      console.error('Form data validation failed:', error);
+      console.error("Form data validation failed:", error);
     }
   };
 
-  const isDisabled = isSubmitting || isValidating;
+  const isDisabled = isSubmitting || isValidating || !isValid;
 
   return (
     <FormProvider {...methods}>
