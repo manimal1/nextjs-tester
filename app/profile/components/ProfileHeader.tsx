57c8570d5dcd2d318efd1917fff4c0e55cadde43
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-15 17:04:16 +0200

    refactor: profile header

--- app/profile/components/ProfileHeader.tsx
+++ app/profile/components/ProfileHeader.tsx
@@ -1,28 +1,23 @@
-import { getUserByEmail } from '@/queries';
-import { getServerSession } from 'next-auth';
-import Image from 'next/image';
+import { authOptions } from "@/app/api";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
 
 export async function ProfileHeader() {
-  const session = await getServerSession();
-  const user = await getUserByEmail(session?.user?.email ?? '');
+  const session = await getServerSession(authOptions);
 
-  if (!session || !user) {
+  if (!session || !session.user) {
     return <p>Sorry, you are not registered!</p>;
   }
+
+  const { image, name, profession } = session.user;
 
   return (
     <>
       <div className="flex items-center space-x-4">
-        <Image
-          src={user.image ?? ''}
-          className="rounded-full w-24 h-24"
-          width={48}
-          height={48}
-          alt="avatar"
-        />
+        <Image src={image ?? ""} className="rounded-full w-24 h-24" width={48} height={48} alt="avatar" />
         <div className="flex flex-col items-start">
-          <h1 className="heading xl:heading-lg">{user.name}</h1>
-          <p className="title-lg text-secondary">{user.profession}</p>
+          <h1 className="heading xl:heading-lg">{name}</h1>
+          <p className="title-lg text-secondary">{profession}</p>
         </div>
       </div>
     </>
