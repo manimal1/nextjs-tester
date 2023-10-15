904b9d89126453aafba4cd8ec0ea8499bd088e07
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-15 17:00:51 +0200

    fix: homepage image icon aspect ration styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function App() {
+  const iconClasses = "rounded-lg h-[80px] w-[80px]";
+
   return (
     <main>
       <header className="flex items-center justify-center w-full">
         <div className="flex flex-col flex-wrap items-end space-x-6 w=[260px]">
-          <Image src="/next-icon.svg" alt="Next.js Logo" width={200} height={200} />
+          <Image src="/next-icon.svg" alt="Next.js Logo" width={200} height={40} className="h-[40px] w-[200px]" />
           <h1 className="heading">Test App</h1>
         </div>
       </header>
@@ -19,61 +21,19 @@ export default function App() {
           alt="TypeScript Logo"
           width={80}
           height={80}
-          className="rounded-lg mt-7"
+          className={`${iconClasses} mt-7`}
         />
-        <Image
-          src="/react-icon.jpeg"
-          alt="React Logo"
-          width={80}
-          height={80}
-          className="rounded-lg"
-        />
+        <Image src="/react-icon.jpeg" alt="React Logo" width={80} height={80} className={iconClasses} />
         <div className="h-[80px] w-[80px] bg-gray-100 rounded-lg flex items-center justify-center">
-          <Image
-            src="/vercel-icon.svg"
-            alt="Vercel Logo"
-            width={50}
-            height={50}
-            className="rounded-lg"
-          />
+          <Image src="/vercel-icon.svg" alt="Vercel Logo" width={50} height={50} className={iconClasses} />
         </div>
-        <Image
-          src="/tailwind-icon.png"
-          alt="Tailwind Logo"
-          width={80}
-          height={80}
-          className="rounded-lg"
-        />
-        <Image
-          src="/rhf-icon.png"
-          alt="React-Hook-Form Logo"
-          width={80}
-          height={80}
-          className="rounded-lg"
-        />
-        <Image src="/zod-icon.png" alt="Zod Logo" width={80} height={80} className="rounded-lg" />
-        <Image
-          src="/playwright-icon.png"
-          alt="Playwright Logo"
-          width={80}
-          height={80}
-          className="rounded-lg"
-        />
-        <Image
-          src="/prisma-icon.jpeg"
-          alt="Prisma Logo"
-          width={80}
-          height={80}
-          className="rounded-lg"
-        />
+        <Image src="/tailwind-icon.png" alt="Tailwind Logo" width={80} height={80} className={iconClasses} />
+        <Image src="/rhf-icon.png" alt="React-Hook-Form Logo" width={80} height={80} className={iconClasses} />
+        <Image src="/zod-icon.png" alt="Zod Logo" width={80} height={80} className={iconClasses} />
+        <Image src="/playwright-icon.png" alt="Playwright Logo" width={80} height={80} className={iconClasses} />
+        <Image src="/prisma-icon.jpeg" alt="Prisma Logo" width={80} height={80} className={iconClasses} />
         <div className="h-[80px] w-[80px] bg-gray-100 rounded-lg flex items-center justify-center">
-          <Image
-            src="/nextauth-icon.png"
-            alt="Next Auth Logo"
-            width={50}
-            height={50}
-            className="rounded-lg"
-          />
+          <Image src="/nextauth-icon.png" alt="Next Auth Logo" width={50} height={50} className={iconClasses} />
         </div>
       </div>
     </main>
