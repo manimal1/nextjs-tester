d9a2fb12f9aea454dffc8a2c41bbedeec8a500b3
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-15 17:03:05 +0200

    fix: pass auth options to server session in protected route

--- app/components/connection/ProtectedRoute.tsx
+++ app/components/connection/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
-import { getServerSession } from 'next-auth';
+import { authOptions } from "@/app/api";
+import { getServerSession } from "next-auth";
 
 export async function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
     return (
       <>
         <h1 className="heading mb-4">Access Denied</h1>
         <p className="title-lg">
-          this is a protected route. You need to{' '}
+          This is a protected route. You need to{" "}
           <a href="/api/auth/signin" className="text-primary">
             Login to continue
           </a>
