cdc2257752257e750fcc9ac7565be1382f2f38c6
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2023-10-15 16:59:57 +0200

    feat: pass authoptions to server session in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from 'next';
-import { getServerSession } from 'next-auth';
-import { Inter } from 'next/font/google';
-import { PageLayout } from '@/ui';
-import { Navbar } from './components';
-import SessionProvider from './components/providers/SessionProvider';
-import '@/ui/theme/styles.css';
+import type { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { Inter } from "next/font/google";
+import { PageLayout } from "@/ui";
+import { Navbar } from "./components";
+import SessionProvider from "./components/providers/SessionProvider";
+import "@/ui/theme/styles.css";
+import { authOptions } from "./api";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
-    'Next.js + TypeScript + TailwindCSS + NextAuth.js + Postgres + Prisma + React-hook-form + Zod + Playwright + Vercel',
-  description: 'Test app created to new skills with Next.js and TypeScript',
-  authors: [{ name: 'Jeremiah Mccurdy' }],
+    "Next.js + TypeScript + TailwindCSS + NextAuth.js + Postgres + Prisma + React-hook-form + Zod + Playwright + Vercel",
+  description: "Test app created to learn new skills with Next.js and TypeScript",
+  authors: [{ name: "Jeremiah Mccurdy" }],
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
